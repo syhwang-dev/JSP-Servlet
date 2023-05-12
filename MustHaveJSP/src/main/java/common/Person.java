35b521b72bbfeb1c42c7b972eabaeaef0ae669c3
syhwang-dev <syhwang.yeon@gmail.com>
2023-05-12 18:04:41 +0900

    Scope of Implicit Object

---
+++ MustHaveJSP/src/main/java/common/Person.java
@@ -0,0 +1,36 @@
+package common;
+
+public class Person {
+	private String name;
+	private int age;
+	
+	public Person() {}
+	
+	// 생성자 - 생성자가 있어야 한다는 것이 기본 규약
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
+}
